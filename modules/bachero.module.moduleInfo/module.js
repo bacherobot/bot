8c1568485b11204a669ec3b766cfd1a5e9bd31a3
Johan <johan_stickman@proton.me>
2022-09-25 21:54:49 +0200

    Support d'un système de pages

--- modules/bachero.module.moduleInfo/module.js
+++ modules/bachero.module.moduleInfo/module.js
@@ -3,6 +3,9 @@ const bacheroFunctions = require('../../functions')
 const authorizedIds = bacheroFunctions.config.getValue('bachero.module.moduleInfo', 'authorizedIds')
 var allModulesDetails
 
+var listModules = []
+var pages = ['<ne doit pas être accéder, signaler ce problème.>'] // Faire que le premier élément (0) soit vide, pour que la liste commence à 1
+
 module.exports = {
 	// Définir les infos de la commande slash
 	slashInfo: new SlashCommandBuilder()
@@ -10,6 +13,9 @@ module.exports = {
 		.setDescription('Affiche la liste des modules')
 		.addStringOption(option => option.setName('packagename')
 			.setDescription('Affiche des informations sur un module en particulier')
+			.setRequired(false))
+		.addNumberOption(option => option.setName('page')
+			.setDescription("Permet de choisir la page affichée lorsqu'on n'entre pas de nom de module")
 			.setRequired(false)),
 
 	// Code a executer quand la commande est appelée
@@ -23,17 +29,34 @@ module.exports = {
 		// Obtenir le nom de packet
 		var packageName = interaction.options.getString('packagename')
 
-		// Obtenir la liste des modules
-		var modules = Object.values(Object.fromEntries(allModulesDetails))
+		// Obtenir la liste des modules si elle n'a pas encore été défini
+		if(!listModules?.length) listModules = Object.values(Object.fromEntries(allModulesDetails))
+
+		// Diviser la liste en plusieurs pages
+		var modulesMessage = ""
+		if(!pages?.[1]) listModules.forEach(mod => {
+			if(modulesMessage.length > 3900){
+				pages.push(modulesMessage)
+				modulesMessage = `\n• *${mod.packageName}* : ${mod.shortDescription.replace(/`/g, '')}`
+			} else modulesMessage += `\n• *${mod.packageName}* : ${mod.shortDescription.replace(/`/g, '')}`
+		})
+		if(modulesMessage) pages.push(modulesMessage)
 
 		// Si aucun nom de packet n'est donné, obtenir la liste des modules
 		if(!packageName){
-			// TODO: créé un hastebin si la liste est trop longue et dépasse la limite de caractères
 			// Créér un embed pour afficher la liste des modules
 			var embed = new EmbedBuilder()
 			.setTitle('Liste des modules')
-			.setDescription(`${modules.length} modules ont été trouvés :\n\n• ${modules.map(m => '*' + m.packageName + '* : ' + m.shortDescription.replace(/`/g, "`" + String.fromCharCode(8203))).join('\n• ')}`.substring(0, 4000))
 			.setColor(bacheroFunctions.config.getValue('bachero', 'embedColor'))
+
+			// Si on veut choisir une page à afficher
+			var currentlyShowedPage = interaction.options.getNumber('page') || 1
+			if(currentlyShowedPage > pages?.length - 1) embed.setFooter({ text: `Première page affichée car la valeur que vous avez spécifiée est trop haute` })
+			else if(currentlyShowedPage < 1) embed.setFooter({ text: `Première page affichée car la valeur que vous avez spécifiée est trop basse` })
+			else embed.setFooter({ text: `Affichage de la page ${currentlyShowedPage}/${pages?.length - 1} • ${listModules?.length} résultats` })
+
+			// Ajouter la page dans l'embed
+			embed.setDescription(pages[currentlyShowedPage] || pages[1] || pages[0] || pages?.join('\n') || pages) // le choix de la sécurité
 
 			// Répondre avec l'embed
 			interaction.reply({ embeds: [embed] }).catch(err => {})
@@ -41,13 +64,13 @@ module.exports = {
 		// Sinon, obtenir les infos sur le module
 		else {
 			// Obtenir le module
-			var module = modules.find(m => m.packageName == packageName)
+			var module = listModules.find(m => m.packageName == packageName)
 
 			// Si le module n'existe pas, répondre avec un message d'erreur
 			if(!module){
 				var embed = new EmbedBuilder()
 				.setTitle('Module introuvable')
-				.setDescription(`Aucun module avec le nom \`${packageName.replace(/`/g, "`" + String.fromCharCode(8203))}\` n'a été trouvé. Utiliser la commande sans arguments pour obtenir la liste complète.`)
+				.setDescription(`Aucun module avec le nom \`${packageName.replace(/`/g, '')}\` n'a été trouvé. Utiliser la commande sans arguments pour obtenir la liste complète.`)
 				.setColor(bacheroFunctions.config.getValue('bachero', 'embedColor'))
 				return interaction.reply({ embeds: [embed] }).catch(err => {})
 			}
@@ -55,7 +78,7 @@ module.exports = {
 			// Sinon, créer un embed pour afficher les infos du module
 			var embed = new EmbedBuilder()
 			.setTitle(module.name)
-			.setDescription(`> ${module.shortDescription.replace(/\n/g, '  ').replace(/`/g, "`" + String.fromCharCode(8203))}\n\n**Nom de packet :** ${module.packageName}\n**Auteur${module.authors.length > 1 ? 's' : ''} :** ${module.authors.join(', ')}\n**Commande${module.commands.length > 1 ? 's' : ''} :** ${(module?.commands?.length > 0 ? module.commands : [{name:'Aucune'}]).map(c => c.name).join(', ')}`)
+			.setDescription(`> ${module.shortDescription.replace(/\n/g, '  ').replace(/`/g, '')}\n\n**Nom de packet :** ${module.packageName}\n**Auteur${module.authors.length > 1 ? 's' : ''} :** ${module.authors.join(', ')}\n**Commande${module.commands.length > 1 ? 's' : ''} :** ${(module?.commands?.length > 0 ? module.commands : [{name:'Aucune'}]).map(c => c.name).join(', ')}`)
 			.setColor(bacheroFunctions.config.getValue('bachero', 'embedColor'))
 			if(module.source && typeof module.source == 'string') embed.setURL(module.source)
 
