28f3dd6bbe92c36b877623ed0eca990e38238c2c
Johan <johan_stickman@proton.me>
2023-07-17 17:11:38 +0200

    Ajout de Stend dans AutoLink

--- modules/bachero.module.autolink/autolink.js
+++ modules/bachero.module.autolink/autolink.js
@@ -1,7 +1,19 @@
 const { EmbedBuilder } = require('discord.js')
 const fetch = require('node-fetch')
+const escape = require('markdown-escape')
 const bacheroFunctions = require('../../functions')
 const database = bacheroFunctions.database.getDatabase('bachero.module.autolink')
+const stendApis = bacheroFunctions.config.getValue('bachero.module.autolink', 'stendApis')
+
+// Convertir une taille en bytes en une taille lisible
+function formatBytes(bytes, decimals = 2){
+	if(!bytes) return '0 B'
+	const k = 1000
+	const dm = decimals < 0 ? 0 : decimals
+	const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+	const i = Math.floor(Math.log(bytes) / Math.log(k))
+	return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
+}
 
 // Fonction pour obtenir des informations sur un lien
 var cachedData = {}
@@ -10,6 +22,9 @@ async function getLinkData(link){
 	var parsedURL = new URL(link)
 	var cleanURL = parsedURL.origin + parsedURL.pathname
 
+	// Si c'est une instance Stend, on ajoute le sharekey dans la cleanURL
+	if(stendApis[parsedURL.host]) cleanURL += parsedURL.search
+
 	// Si on a déjà des informations sur ce lien, on les renvoie
 	if(cachedData[cleanURL]){
 		cachedData[cleanURL].lastUsed = Date.now()
@@ -17,7 +32,7 @@ async function getLinkData(link){
 	}
 
 	// Sinon, on fait une requête
-	if(parsedURL.host.startsWith('github.com')){
+	if(parsedURL.host.startsWith('github.com')){ // GitHub
 		// Obtenir l'URL de l'API à fetch
 		var pathnameSplit = parsedURL.pathname.split('/')
 		if(pathnameSplit.length < 2) return
@@ -32,7 +47,7 @@ async function getLinkData(link){
 		else if(apiData?.name) cachedData[cleanURL] = { lastUsed: Date.now(), data: { platform: 'github', type: 'repo', name: apiData?.name, description: apiData?.description?.trim(), author: apiData?.owner?.login, html_url: apiData?.html_url } }
 		else if(apiData?.login) cachedData[cleanURL] = { lastUsed: Date.now(), data: { platform: 'github', type: 'user', login: apiData?.login, email: apiData?.email, name: apiData?.name, bio: apiData?.bio, html_url: apiData?.html_url } }
 	}
-	if(parsedURL.host.startsWith('gist.github.com')){
+	if(parsedURL.host.startsWith('gist.github.com')){ // GitHub Gist
 		// Obtenir l'URL de l'API à fetch
 		var pathnameSplit = parsedURL.pathname.split('/')
 		if(pathnameSplit[pathnameSplit.length-1] == '') pathnameSplit.length = pathnameSplit.length - 1
@@ -42,7 +57,7 @@ async function getLinkData(link){
 		if(apiData?.message && apiData != 'Not Found') return bacheroFunctions.showLog('warn', `Problème rencontré lors d'une vérification avec le module "bachero.module.autolink" : ${apiData?.message || JSON.stringify(apiData)}`, id="autolink-verify-github")
 		if(apiData?.description) cachedData[cleanURL] = { lastUsed: Date.now(), data: { platform: 'github', type: 'gist', description: apiData?.description, comments: apiData?.comments, author: apiData?.owner?.login, html_url: apiData?.html_url  } }
 	}
-	if(parsedURL.host.startsWith('www.npmjs.com')){
+	if(parsedURL.host.startsWith('www.npmjs.com')){ // NPMJS
 		// Obtenir l'URL de l'API à fetch
 		var pathnameSplit = parsedURL.pathname.split('/')
 		if(pathnameSplit[pathnameSplit.length-1] == '') pathnameSplit.length = pathnameSplit.length - 1
@@ -57,8 +72,22 @@ async function getLinkData(link){
 		if(apiData?.error) return bacheroFunctions.showLog('warn', `Problème rencontré lors d'une vérification avec le module "bachero.module.autolink" : ${apiData?.error || JSON.stringify(apiData)}`, id="autolink-verify-npmjs")
 		if(apiData?.name && apiData?.description) cachedData[cleanURL] = { lastUsed: Date.now(), data: { platform: 'npm', name: apiData?.name, description: apiData?.description, author: apiData?.author, html_url: `https://www.npmjs.com/package/${apiData?.name}` } }
 	}
+	if(stendApis != {}){ // Stend
+		// Obtenir le lien de l'API
+		var apiURL = stendApis[parsedURL.host]
 
-	// Si on a des nouvelles données, on les renvoie
+		// Si on a une API pour ce domaine, on y fait une requête pour obtenir les informations du transfert
+		if(apiURL){
+			// Obtenir la clé de partage dans l'URL
+			var shareKey = parsedURL.search.replace('?','')
+
+			// Faire la requête et enregistrer dans le cache
+			var apiData = await fetch(`${apiURL}/files/info?sharekey=${shareKey}`, { headers: { 'User-Agent': 'BacheroBot (+https://github.com/bacherobot/bot)' } }).then(res => res.json()).catch(err => { return {} })
+			if(!apiData?.error && !apiData?.statusCode) cachedData[cleanURL] = { lastUsed: Date.now(), data: { platform: 'stend', isGroup: apiData?.isGroup || false, groups: apiData?.groups || [], fileName: apiData?.fileName, fileSize: formatBytes(apiData?.fileSize), expireDate: apiData?.expireDate, download_url: apiData.downloadLink ? `${apiURL}${apiData.downloadLink}` : null, html_url: `https://${parsedURL.host}/d.html?${shareKey}` } }
+		}
+	}
+
+	// Si on a eu des nouvelles données, on les renvoie
 	if(cachedData[cleanURL]) return cachedData[cleanURL].data
 }
 
@@ -77,7 +106,8 @@ module.exports = {
 
 			// Filtrer pour n'ajouter que les liens de certains domaines
 			if(links?.length) links = links.filter(link => {
-				link = link.replace('https://','')
+				if(Object.keys(stendApis).find(li => li.startsWith(new URL(link).origin.replace('https://','')))) return true
+				link = link.replace('https://','') // pour les liens suivants, on enlève le https://
 				if(link.startsWith('github.com')) return true
 				if(link.startsWith('gist.github.com')) return true
 				if(link.startsWith('www.npmjs.com/package')) return true
@@ -100,6 +130,10 @@ module.exports = {
 				if(info?.platform == 'github' && info?.type == 'issues') description.push(`(GitHub) [${info?.title}](${info?.html_url})\n> Auteur : [${info?.author}](https://github.com/${info?.author?.replace('[bot]','')})\n> Issue ${info?.state?.replace('closed','fermée').replace('open', 'ouverte').replace('completed', 'completée').replace('not_planned', 'non prévue').replace('reopened','réouverte')}\n> <t:${Math.round(new Date(info?.created_at).getTime() / 1000)}:D> → <t:${Math.round(new Date(info?.closed_at).getTime() / 1000)}:D>`.trim())
 				if(info?.platform == 'github' && info?.type == 'gist') description.push(`(GitHub Gist) [${info?.description.substring(0, 250)}](${info?.html_url})\n> Auteur : [${info?.author}](https://github.com/${info?.author?.replace('[bot]','')})${info?.comments ? `\n> ${info?.comments} commentaires` : ''}`.trim())
 
+				// Stend
+				if(info?.platform == 'stend' && info.isGroup) description.push(`(Stend) [Groupe de transferts](${info?.html_url})\n> Contient ${info?.groups?.length} transfert${info?.groups?.length > 1 ? 's' : ''}`.trim()) // si c'est un transfert groupé
+				else if(info?.platform == 'stend') description.push(`(Stend) [${info?.fileName}](${info?.html_url})\n> Taille : ${info?.fileSize}\n> Expire <t:${Math.round(new Date(info?.expireDate).getTime() / 1000)}:R>`.trim()) // .. si ça l'est pas
+
 				// Autres
 				if(info?.platform == 'npm') description.push(`(NPM) [${info?.name}](${info?.html_url})${info?.author && typeof info?.author == 'string' && info?.author?.length ? `\n> Auteur : [${info?.author}](https://www.npmjs.com/~${info?.author?.toLowerCase()})` : ''}${info?.description ? '\n> ' + info?.description.substring(0, 1400) : ''}`.trim())
 			}
@@ -112,7 +146,7 @@ module.exports = {
 			.setTitle(`${links.length > 1 ? 'Des liens ont été détectés' : 'Un lien a été détecté'}`)
 			.setDescription(description.join('\n\n').substring(0, 4000))
 			.setColor(bacheroFunctions.config.getValue('bachero', 'embedColor'))
-			.setFooter({ text: `${description.join('\n\n').length > 2000 ? "Les informations sont tronquées." : "Le contenu de ce message n'est pas vérifié"}. Informations obtenues grâce au message de ${message.author.tag} (ID: ${message.author.id}).` })
+			.setFooter({ text: `${description.join('\n\n').length > 2000 ? "Les informations sont tronquées." : "Le contenu de ce message n'est pas vérifié"}. Informations obtenues grâce au message de ${message.author.discriminator == '0' ? escape(message.author.username) : escape(message.author.tag)} (ID: ${message.author.id}). Via AutoLink.` })
 			message.reply({ embeds: [embed] }).catch(err => {})
 		})
 	}
