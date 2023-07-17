051319b44815bf778d30b1c1d1330ed8bf507c92
Johan <johan_stickman@proton.me>
2023-07-17 17:11:09 +0200

    Update lyrics.js
    Les paroles sont également cherchés dans la musique écouté sur le profil de l'utilisateur
    Les paroles sont modifiés pour être plus court en caractères

--- modules/bachero.module.lyrics/lyrics.js
+++ modules/bachero.module.lyrics/lyrics.js
@@ -1,6 +1,31 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js')
 const bacheroFunctions = require('../../functions')
 const Genius = require("genius-lyrics"); const GeniusClient = new Genius.Client();
+const escape = require('markdown-escape')
+
+// Fonction pour tenter de "compresser" un texte tout en le gardant lisible pour un humain
+function compressText(text){
+	// Remplacer certains caractères par d'autres
+	text = text.replace(/\.\.\./g, '…')
+	text = text.replace(/\.\./g, '‥')
+	text = text.replace(/!!/g, '‼')
+	text = text.replace(/\?\?/g, '⁇')
+	text = text.replace(/!\?/g, '⁈')
+	text = text.replace(/\?!/g, '⁉')
+	text = text.replace(/--/g, '—')
+	text = text.replace(/---/g, '——')
+	text = text.replace(/----/g, '———')
+	text = text.replace(/-----/g, '———')
+	
+	// Enlever les indications de paroles (toute les lignes qui commencent par un [ et fini par un ])
+	text = text.replace(/\[.*\]/g, '')
+
+	// Enlever les doubles sauts de ligne
+	text = text.replace(/\n\n/g, '\n')
+
+	// Retourner le texte
+	return text.trim()
+}
 
 module.exports = {
 	// Définir les infos de la commande slash
@@ -9,7 +34,7 @@ module.exports = {
 		.setDescription('Recherche et obtient les paroles d\'une chanson')
 		.addStringOption(option => option.setName('search')
 			.setDescription("Terme de recherche")
-			.setRequired(true)
+			.setRequired(false)
 		),
 
 	// Code a executer quand la commande est appelée
@@ -17,29 +42,42 @@ module.exports = {
 		// Mettre la réponse en defer
 		if(await interaction.deferReply().catch(err => { return 'stop' }) == 'stop') return
 
-		// Obtenir le terme de recherche, et en faire une recherche
-		var searchResult = await GeniusClient.songs.search(interaction.options.getString('search'))
+		// Obtenir le terme de recherche
+		var query = interaction.options.getString('search')
+
+		// Si on a pas de terme de recherche, on va chercher ce que l'utilisateur est en train d'écouter
+		if(!query && interaction.member?.presence?.activities?.length){
+			var listening = interaction.member?.presence?.activities.find(a => a.type == 2 && a?.details?.length && a?.state?.length)
+			if(listening) query = `${listening.details} ${listening.state}`
+		}
+
+		// Si on a toujours pas de terme de recherche, on affiche une erreur
+		if(!query) return interaction.editReply("Pour utiliser cette commande, vous devez inclure l'argument `search` dans votre commande, ou disposez d'un statut d'écoute sur votre profil Discord (ne fonctionne que sur un serveur).").catch(err => {})
+
+		// Faire une recherche sur Genius
+		var searchResult = await GeniusClient.songs.search(query)
 		if(!searchResult?.length) return interaction.editReply({ content: "Aucun résultat n'a pu être trouvé pour ce terme de résultat." }).catch(err => {})
 		searchResult = searchResult[0]
 
 		// Obtenir les paroles
 		var lyrics = await searchResult.lyrics()
 		if(!lyrics) return interaction.editReply({ content: "Aucune paroles n'a pu être trouvé pour ce terme de résultat." }).catch(err => {})
+		lyrics = compressText(lyrics)
 
 		// Créer l'embed
 		var embed = new EmbedBuilder()
 		.setTitle(searchResult.fullTitle)
-		.setDescription(lyrics.substring(0, 4092).length < lyrics.length ? lyrics.substring(0, 4092) + '...' : lyrics)
+		.setDescription(lyrics.substring(0, 4094).length < lyrics.length ? lyrics.substring(0, 4094) + '…' : lyrics)
 		.setThumbnail(searchResult.thumbnail)
 		.setColor(bacheroFunctions.config.getValue('bachero', 'embedColor'))
-		.setFooter({ text: `Paroles récupérées via Genius sous la demande de ${interaction.user.tag}` })
+		.setFooter({ text: `Paroles récupérées via Genius sous la demande de ${interaction.user.discriminator == '0' ? escape(interaction.user.username) : escape(interaction.user.tag)}` })
 
 		// Créer un bouton
 		var row = new ActionRowBuilder().addComponents(
 			new ButtonBuilder()
 			.setURL(searchResult.url)
 			.setStyle(ButtonStyle.Link)
-			.setLabel(`Voir ${lyrics.substring(0, 4092).length < lyrics.length ? 'en entier' : ''} sur Genius`)
+			.setLabel(`Voir ${lyrics.substring(0, 4092).length < lyrics.length ? 'en entier ' : ''}sur Genius`)
 		)
 
 		// Envoyer l'embed
