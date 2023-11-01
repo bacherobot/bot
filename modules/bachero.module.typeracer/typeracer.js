2a74839faaf3f3d62ad42c48f52eabc34e81effc
Johan le stickman <Johan_Stickman@proton.me>
2023-11-01 17:49:15 +0100

    Nouvelle quotes, ajout de tips, suppr. vérif. sauts de ligne

--- modules/bachero.module.typeracer/typeracer.js
+++ modules/bachero.module.typeracer/typeracer.js
@@ -48,7 +48,6 @@ const listPhrases = [
 	{ text: "La voix de la sagesse est muette, elle parle en langage des signes", source: "https://genius.com/Damso-javais-juste-envie-decrire-lyrics" },
 	{ text: "Sentiments égarés, on sourit pour ne pas pleurer", source: "https://genius.com/Damso-silence-lyrics" },
 	{ text: "On s'oublie pour ne plus s'aimer, cherche la perfection pour fuir la réalité", source: "https://genius.com/Damso-silence-lyrics" },
-	{ text: "Le vrai problème, c'est qu'à chaque fois qu'c'est le même problème", source: "https://genius.com/Damso-silence-lyrics" },
 	{ text: "Je ne vois plus que des clones, ça a commencé à l'école\nÀ qui tu donnes de l'épaule pour t'en sortir ?", source: "https://genius.com/Nekfeu-nique-les-clones-part-ii-lyrics" },
 	{ text: "Ici, tout l'monde joue des rôles en rêvant du million d'euros\nEt j'ai poussé comme une rose parmi les orties", source: "https://genius.com/Nekfeu-nique-les-clones-part-ii-lyrics" },
 	{ text: "T'es malheureux quand t'as qu'un rêve et que tes parents ne veulent pas", source: "https://genius.com/Nekfeu-nique-les-clones-part-ii-lyrics" },
@@ -65,6 +64,58 @@ const listPhrases = [
 	{ text: "Pardonne-moi si j'suis une personne trop dure à aimer", source: "https://genius.com/Jokair-las-vegas-lyrics" },
 	{ text: "J'accélère, on a la tête posée sur l'appuie-tête\nToute la nuit, les pneus brûlent sur la route 66", source: "https://genius.com/Jokair-las-vegas-lyrics" },
 	{ text: "Au retour, mes draps avaient encore son odeur\nDur d'effacer nos souvenirs de ma tête", source: "https://genius.com/Jokair-nos-souvenirs-lyrics" },
+	{ text: "J'aimerais tellement te prêter mes yeux\nQue tu puisses voir le feu qui sommeille en toi", source: "https://genius.com/Luidji-reste-en-vie-lyrics" },
+	{ text: "N'oublie jamais ta mission\nPour un peu d'amour, un peu d'attention\nSurtout si tu ne vis pas encore du son, ou de ta passion", source: "https://genius.com/Luidji-reste-en-vie-lyrics" },
+	{ text: "Trouve un job, arrondis tes fins de mois\nAssume toi c'est le premier pas", source: "https://genius.com/Luidji-reste-en-vie-lyrics" },
+	{ text: "Est-c'qu'un jour j'pourrais compter parmi les grands de ce monde ? J'ai toujours voulu compter parmi les grands de ce monde", source: "https://genius.com/Luidji-monde-lyrics" },
+	{ text: "Esclave de ma cause mais j'ai choisi mes chaînes", source: "https://genius.com/Luidji-monde-lyrics" },
+	{ text: "Et quand l'FAMAS blesse, les égos se taisent\nEt quand l'témoin s'lève, j'suis toujours à l'aise", source: "" },
+	{ text: "Faut qu'j'me pète une caisse assortie à mes textes", source: "https://genius.com/Laylow-maladresse-lyrics" },
+	{ text: "Tous ces moments où t'es seul et tu t'sens humilié", source: "https://genius.com/Laylow-maladresse-lyrics" },
+	{ text: "Démon me chuchote à l'oreille : mais démolis-les", source: "https://genius.com/Laylow-maladresse-lyrics" },
+	{ text: "Démon m'dit qu'il faut rien oublier, toutes les défaites et les ennemis d'hier", source: "https://genius.com/Laylow-maladresse-lyrics" },
+	{ text: "J'suis plus dans l'93, on a changé d'adresse", source: "https://genius.com/Laylow-maladresse-lyrics" },
+	{ text: "J'suis dans l'game et j'vois mon corps en pleine élévation\nAprès l'but, j'tire dans la foule pour la célébration", source: "https://genius.com/Luv-resval-celebration-lyrics" },
+	{ text: "C'est qui cette fille ? Qui s'prend en selfie, seule sur le ponton, j'sais même plus dans quelle vie", source: "https://genius.com/Luv-resval-cette-fille-lyrics" },
+	{ text: "Pour connaître la nuit dans ses draps, toute la nuit, tu mènes l'enquête", source: "https://genius.com/Luv-resval-cette-fille-lyrics" },
+	{ text: "Pendant qu'les anges nettoient la harpe, Satan joue des airs de trompette", source: "https://genius.com/Luv-resval-cette-fille-lyrics" },
+	{ text: "Elle pleure, elle donne sa rose, un regard, le temps s'arrête, toutes les fleurs s'arrosent", source: "https://genius.com/Luv-resval-cette-fille-lyrics" },
+	{ text: "C'est qui cette fille ? Dans la soirée, dans un métro, tu l'as croisée comme un écho\nLes yeux dans l'vide, les étoiles en train d'danser, t'aurais tout donné pour savoir à qui elle pensait", source: "https://genius.com/Luv-resval-cette-fille-lyrics" },
+	{ text: "J'ressens tristesse et haine, les deux vont s'mélanger", source: "https://genius.com/Savage-toddy-tout-sen-va-sauf-les-souvenirs-lyrics" },
+	{ text: "Comme d'habitude, on voulait des habits d'luxe, des rides pour kiffer dans cette vie\nLes choses n'ont d'jà plus la même saveurs, le même prix", source: "https://genius.com/Savage-toddy-tout-sen-va-sauf-les-souvenirs-lyrics" },
+	{ text: "Ils ignorent c'que la misère c'est, parle à mon cœur pour voir c'que l'hiver fait", source: "https://genius.com/Savage-toddy-tout-sen-va-sauf-les-souvenirs-lyrics" },
+	{ text: "Toujours plus, pourquoi toujours plus ?\nToujours plus, j'en veux toujours plus", source: "https://genius.com/Savage-toddy-tout-sen-va-sauf-les-souvenirs-lyrics" },
+	{ text: "Il m'avait parlé d'elle, elle s'appelait Mariah\nSon parfum, c'était Vanilla, elle ressemblait à Alia", source: "https://genius.com/Savage-toddy-tout-sen-va-sauf-les-souvenirs-lyrics" },
+	{ text: "Je suis tombé de tellement haut je pourrais jamais tomber plus bas", source: "https://genius.com/Savage-toddy-tout-sen-va-sauf-les-souvenirs-lyrics" },
+	{ text: "T'avais pas peur du risque, non, toi, t'avais pas peur de l'oseille\nJ'te suivais partout avec les mêmes lunettes que Kurt Cobain", source: "https://genius.com/Savage-toddy-tout-sen-va-sauf-les-souvenirs-lyrics" },
+	{ text: "Tu nous as rappelé que la vie n'a aucun sens, que le temps passe et tout s'en va", source: "https://genius.com/Savage-toddy-tout-sen-va-sauf-les-souvenirs-lyrics" },
+	{ text: "T'as vu passer plus que sept filles mais tu parlais que de cette fille", source: "https://genius.com/Savage-toddy-tout-sen-va-sauf-les-souvenirs-lyrics" },
+	{ text: "Les scènes que j'vis me rendent plus sensible à force", source: "https://genius.com/Savage-toddy-tout-sen-va-sauf-les-souvenirs-lyrics" },
+	{ text: "T'étais différent des autres, et ça, une mère le voit avant qu'le monde puisse s'en apercevoir", source: "https://genius.com/Savage-toddy-tout-sen-va-sauf-les-souvenirs-lyrics" },
+	{ text: "J'oublie pas ton entrainement, mon reuf. Repose en paix mon ange", source: "https://genius.com/Savage-toddy-tout-sen-va-sauf-les-souvenirs-lyrics" },
+	{ text: "J'tourne en rond, j'ai plus sommeil, j'gratte un couplet dans la fumée", source: "https://genius.com/Luv-resval-hades-lyrics" },
+	{ text: "J'ai plus rien à dire au soleil, j'aime toujours autant m'isoler", source: "https://genius.com/Luv-resval-hades-lyrics" },
+	{ text: "La déesse est si belle, donc on l'a suivi sur les sentiers", source: "https://genius.com/Luv-resval-hades-lyrics" },
+	{ text: "Et mes textes se ressemblent, c'est la Terre qui me chante, ça n'a pas d'sens", source: "https://genius.com/Luv-resval-hades-lyrics" },
+	{ text: "Dans l'ivresse, on encaisse et les troubles se ressentent", source: "https://genius.com/Luv-resval-hades-lyrics" },
+	{ text: "Que si la vie ne m'donne pas tout c'que j'lui demande, j'passerai par la violence", source: "https://genius.com/Damso-tueurs-lyrics" },
+	{ text: "Chérie, j'suis pas dispo, pourquoi tu m'appelles ?", source: "https://genius.com/Luther-lutherantz-pk-tu-mapl-lyrics" },
+	{ text: "Avant, j'attendais tes messages comme le jour de la paye\nMais tout ça, c'est du passé, mon cœur est cassé", source: "https://genius.com/Luther-lutherantz-pk-tu-mapl-lyrics" },
+	{ text: "J'suis qu'un artiste qui comprend pas c'que vous peignez sur vos toiles", source: "https://genius.com/Winnterzuko-je-suis-musik-lyrics" },
+	{ text: "Même quand j'étais l'plus nul de la bande\nMême quand ils avaient déjà beaucoup d'avance", source: "https://genius.com/Winnterzuko-je-suis-musik-lyrics" },
+	{ text: "J'ai trop misé comme si j'avais rien à perdre\nJ'les vois m'regarder comme si j'étais sous l'averse\nJ'dois faire honneur à tous ceux qui sont sous la terre", source: "https://genius.com/Winnterzuko-je-suis-musik-lyrics" },
+	{ text: "J'viens pas à ta soirée sauf si c'est pour venir à ta rescousse\nJ'suis rempli d'imperfections mais mon cœur, faut qu'on laisse tout", source: "" },
+	{ text: "C'est quand je me retrouve tout seul que j'suis plus tout seul dans ma tête", source: "https://genius.com/Winnterzuko-enfer-et-paradis-lyrics" },
+	{ text: "C'est bien là tu t'sens à l'aise, ils n'attendaient qu'ça pour t'la mettre", source: "https://genius.com/Winnterzuko-enfer-et-paradis-lyrics" },
+	{ text: "Ce soir ça va mal s'finir mais demain j'recommence le jeu", source: "https://genius.com/Winnterzuko-enfer-et-paradis-lyrics" },
+	{ text: "Me dis pas qu't'es mon frère si c'est pas vrai\nMe dis pas qu't'es prêt à bouger si t'es pas prêt", source: "https://genius.com/Winnterzuko-115-lyrics" },
+	{ text: "Et c'est pas Merlin qui m'enchante\nEt quand j'chante naïvement, j'espère que les gens chantent", source: "https://genius.com/Winnterzuko-115-lyrics" },
+	{ text: "J'suis né pendant la guerre, ma mère s'est battue pour mon enfance", source: "https://genius.com/Winnterzuko-115-lyrics" }
+]
+const tips = [
+	"Les sauts de lignes, majuscules et accents manqués ne sont pas comptés comme des erreurs",
+	"Le score prend en compte le temps de réponse et le nombre d'erreurs",
+	`On a que ${listPhrases.length} phrases pour le moment, tu peux nous en proposer de nouvelles en nous contactant !`
 ]
 
 // Exporter certaines fonctions
@@ -80,7 +131,7 @@ module.exports = {
 
 	// Définir les infos du menu contextuel
 	contextInfo: new ContextMenuCommandBuilder()
-		.setName("Défier sur typeracer")
+		.setName("Défier sur Typeracer")
 		.setType(ApplicationCommandType.User),
 
 	// Code à exécuter quand la commande est appelée
@@ -97,7 +148,7 @@ module.exports = {
 		// Générer un embed
 		const embed = new EmbedBuilder()
 			.setTitle("Demande de duel d'écriture")
-			.setDescription(`${opponentMention}, **${interaction.user.discriminator == "0" ? escape(interaction.user.username) : escape(interaction.user.tag)}** vous défie à un duel d'écriture !\n\nUn texte sera envoyé, le premier à le recopier et l'envoyer dans ce salon deviendra vainqueur !`)
+			.setDescription(`${opponentMention}, **${interaction.user.discriminator == "0" ? escape(interaction.user.username) : escape(interaction.user.tag)}** vous défie à un duel d'écriture !\n\nUn texte sera envoyé, le premier à le recopier et l'envoyer dans ce salon deviendra vainqueur !\n\n> **Tips :** ${rando(tips).value}`)
 			.setFooter({ text: "Vous avez 30 secondes pour accepter cette demande" })
 			.setColor(colors.primary)
 
@@ -144,7 +195,7 @@ module.exports = {
 			}
 
 			// Envoyer le message
-			await interaction.editReply({ embeds: [embed.setTitle(`${interaction.user.username} VS ${opponent.username}`).setDescription(`> ${phraseAntiCP.replaceAll("\n", "\n> ")}`).setColor(colors.secondary)] }).catch(err => {})
+			await interaction.editReply({ embeds: [embed.setTitle(`${interaction.user.displayName.length ? escape(interaction.user.displayName) : interaction.user.username} VS ${opponent.displayName.length ? escape(opponent.displayName) : opponent.username}`).setDescription(`> ${phraseAntiCP.replaceAll("\n", "\n> ")}`).setColor(colors.secondary)] }).catch(err => {})
 			var dateStartGame = Date.now()
 
 			// Attendre une réponse
@@ -176,12 +227,14 @@ module.exports = {
 					var taken2 = parseFloat(((message2.createdTimestamp - dateStartGame) / 1000).toFixed(2))
 
 					// Modifier le contenu des messages pour remplacer les caractères invisibles
-					message1.content = message1.content.replace(/[\u200B\u200C\u200D\u2060\uFEFF]/g, "*") // eslint-disable-line
-					message2.content = message2.content.replace(/[\u200B\u200C\u200D\u2060\uFEFF]/g, "*") // eslint-disable-line
+					for(var i = 0; i < invisibleChars.length; i++){
+						message1.content = message1.content.replaceAll(invisibleChars[i], "")
+						message2.content = message2.content.replaceAll(invisibleChars[i], "")
+					}
 
 					// Déterminer le nombre d'erreurs dans le texte
-					var errors1 = diffWords(message1.content.replace(/[^a-zA-Z0-9\*', çéêèà]/g, ""), phrase.text.replace(/[^a-zA-Z0-9', çéêèà\*]/g, ""), { ignoreCase: true }).length - 1 // eslint-disable-line
-					var errors2 = diffWords(message2.content.replace(/[^a-zA-Z0-9\*', çéêèà]/g, ""), phrase.text.replace(/[^a-zA-Z0-9', çéêèà\*]/g, ""), { ignoreCase: true }).length - 1 // eslint-disable-line
+					var errors1 = diffWords(message1.content.replace(/[^a-zA-Z0-9\*', çéêèà]/g, "").replace(/\n| |œ/g, ''), phrase.text.replace(/[^a-zA-Z0-9', çéêèà\*]/g, "").replace(/\n| |œ/g, ''), { ignoreCase: true }).length - 1 // eslint-disable-line
+					var errors2 = diffWords(message2.content.replace(/[^a-zA-Z0-9\*', çéêèà]/g, "").replace(/\n| |œ/g, ''), phrase.text.replace(/[^a-zA-Z0-9', çéêèà\*]/g, "").replace(/\n| |œ/g, ''), { ignoreCase: true }).length - 1 // eslint-disable-line
 
 					// Calculer un score en fonction du temps et du nombre d'erreurs
 					var score1 = parseFloat((taken1 + (errors1 * 2)).toFixed(2))
